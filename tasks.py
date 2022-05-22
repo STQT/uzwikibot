ad22aec77be4d0cc63ab200b7813457f97a4ba57
Gayrat <gayratbek.sultonov@gmail.com>
2022-05-22 22:49:01 +0500

    fountain message changes

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from utils import WikiApi
 
@@ -11,7 +11,7 @@ channel_link = "@uzwikinewpages"
 
 async def get_last_created_page(bot, collwikis):
     data = api.get_new_pages()
-
+    fountain_link = "https://fountain.toolforge.org/editathons/wiki-stipendiya-marafoni"
     recent_list: list = data['query']['recentchanges']
     for i in recent_list:
         i.update({
@@ -30,22 +30,32 @@ async def get_last_created_page(bot, collwikis):
     for x in recent_list:
         if x.get('pageid') not in recent_db_ids:
             new_page_obj.append(x)
-    from datetime import datetime
     if new_page_obj:
         await collwikis.insert_many(
             [i for i in new_page_obj])
         for page in new_page_obj:
             vaqt = datetime.strptime(page['timestamp'], "%Y-%m-%dT%H:%M:%SZ") + timedelta(hours=5)
-            await bot.send_message(channel_link, "<b>Yangi sahifa!</b>\n"
-                                                 "Nomi: <a href=\"https://uz.wikipedia.org/wiki/{title}/\">"
-                                                 "{title}</a>\n"
-                                                 "Foydalanuvchi: {user}\n"
-                                                 "Hajmi: {newlen}\n"
-                                                 "Vaqti: {vaqt}".format(title=page['title'],
-                                                                        user=page['user'],
-                                                                        newlen=page['newlen'],
-                                                                        vaqt=vaqt.strftime("%H:%M:%S")),
-                                   parse_mode="HTML", disable_web_page_preview=True)
+            if page['newlen'] != 27:
+                await bot.send_message(channel_link, "<b>Yangi sahifa!</b>\n"
+                                                     "Nomi: <a href=\"https://uz.wikipedia.org/wiki/{title}\">"
+                                                     "{title}</a>\n"
+                                                     "Foydalanuvchi: {user}\n"
+                                                     "Hajmi: {newlen}\n"
+                                                     "Vaqti: {vaqt}".format(title=page['title'],
+                                                                            user=page['user'],
+                                                                            newlen=page['newlen'],
+                                                                            vaqt=vaqt.strftime("%H:%M:%S")),
+                                       parse_mode="HTML", disable_web_page_preview=True)
+            else:
+                await bot.send_message(channel_link, "<a href=\"{fountain_link}\">Fountainda yangi maqola</a><\n"
+                                                     "Nomi: <a href=\"https://uz.wikipedia.org/wiki/{title}\">"
+                                                     "{title}</a>\n"
+                                                     "Foydalanuvchi: {user}\n"
+                                                     "Vaqti: {vaqt}".format(title=page['title'].replace('Munozara:', ''), # noqa
+                                                                            user=page['user'],
+                                                                            vaqt=vaqt.strftime("%H:%M:%S"),
+                                                                            fountain_link=fountain_link),
+                                       parse_mode="HTML", disable_web_page_preview=True)
     else:
         logging.info("NO CHANGES")
 
